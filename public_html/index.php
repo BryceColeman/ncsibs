83a1e331782b1efd778c2d85214244b4c57cfcb5
Bryce Coleman <coleman.bryce@gmail.com>
2017-10-13 23:41:00 -0400

    Added cards to home page for navigation

--- public_html/index.php
+++ public_html/index.php
@@ -31,9 +31,10 @@
                             <h4 style="color:#c03a2b">
                               Upcoming Events!
                     </h4>
-                  </div>
+                        </div>
                         <p style="padding-bottom: 15px;">
-                            Join us on Saturday, October 14 in Greensboro, NC for a NC Sibs event. <a href="files/oct-14-social.pdf">Click here</a> for the flyer. Don't forget to visit our <a href="https://www.meetup.com/ncsibs/">Meetup page</a> for more details!
+                            Join us on Saturday, October 14 in Greensboro, NC for a NC Sibs event. <a href="files/oct-14-social.pdf">Click here</a> for the flyer. Don't forget to visit our <a href="https://www.meetup.com/ncsibs/">Meetup page</a> for more
+                            details!
                         </p>
 
                     </div>
@@ -43,34 +44,40 @@
 
     </div>
 
+    <div class="row" style="margin: 20px;">
+        <div class="col s12 m12 l12" style="padding: 75px 0px 75px 0px;">
+            <div class="row">
+                <div class="col s12 m4 l4">
+                  <div class="box-border">
 
-    <div class="row tri-content">
+                    <a href="/get-involved.php"><img src="img/icon-set/heart.svg" class="icon" alt=""></a>
+                    <a href="/get-involved.php"><h4>Support</h4></a>
+                    <p id="card-text">Help us grow and maintain the network. If you have event space for a sibling meetup, are interested in presenting some useful information to the network, or would like to help in some other way then we would love to hear from you!
+                      Contact us using the links below or take a look at our <a href="\get-involved.php"> get involved </a>section.</p>
+                  </div>
+                </div>
+                <div class="col s12 m4 l4">
+                    <div class="box-border">
+                        <a href="/resources.php"><img src="img/icon-set/binoculars.svg" class="icon" alt=""></a>
+                        <a href="/resources.php"><h4>Resources</h4></a>
+                        <p id="card-text">Search the resources we've gathered for siblings of North Carolinians with developmental disabilities. Learn more about a host of useful topics. You can
+                            even take a look at our growing list of <a href="/resources.php#faq">frequently asked questions.
+    </a> If there are resources you want added to the site please contact us using the links below.</p>
 
-        <div class="box col s12 m12 l12">
+                    </div>
+                </div>
+                <div class="col s12 m4 l4">
+                  <div class="box-border">
 
-          <div class="box-content col s12 m4 l4">
-            <a href="/get-involved.php"><img src="img/icon-set/heart.svg" class="icon" alt=""></a>
-            <a href="/get-involved.php"><h4>Support</h4></a>
-            <p>Help us grow and maintain the network. If you have event space for a sibling meetup, are interested in presenting some useful information to the network, or would like to help in some other way then we would love to hear from you! Contact
-              us using the links below or take a look at our <a href="\get-involved.php"> get involved </a>section.</p>
+                    <a href="/get-involved.php"><img src="img/icon-set/community.svg" class="icon" alt=""></a>
+                    <a href="/get-involved.php"><h4>Connect</h4></a>
+                    <p id="card-text">Connect with NC Sibs. Whether you're a sibling, parent, professional, self-advocate, or even a friend or neighbor, we would love for you to connect with and be involved in the NC Sibs network. You can attend an event, join a conference call, or meet up with a sibling in your area. <a href="/get-involved.php">Learn more about connecting.</a></p>
+                  </div>
+                </div>
             </div>
-            
-            <div class="box-content col s12 m4 l4">
-                <a href="/resources.php"><img src="img/icon-set/binoculars.svg" class="icon" alt=""></a>
-                <a href="/resources.php"><h4>Resources</h4></a>
-                <p>Search the resources we've gathered for siblings of North Carolinians with developmental disabilities. Learn more about guardianship and it’s alternatives, advocacy and public policy, and a host of other useful topics. You can even take
-                    a look at our growing list of <a href="/resources.php#faq">frequently asked questions.
-                  </a> If there are resources you want added to the site please contact us using the links below.</p>
-            </div>
-
-            <div class="box-content col s12 m4 l4">
-                <a href="/get-involved.php"><img src="img/icon-set/community.svg" class="icon" alt=""></a>
-                <a href="/get-involved.php"><h4>Connect</h4></a>
-                <p>Connect with NC Sibs. Whether you're a sibling, parent, professional, self-advocate, or even a friend or neighbor, we would love for you to be involved in the network. <a href="/get-involved.php">Learn more about connecting with us.</a></p>
-            </div>
-
         </div>
     </div>
+
     <div class="z-depth-4" id="footer">
         <?php include("includes/footer.html"); ?>
     </div>
